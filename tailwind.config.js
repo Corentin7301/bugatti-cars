ba07094a89afe5cc04ade98228bc0201120988a4
corentinperroux <corentin7301@gmail.com>
2021-04-26 12:28:33 +0100

    add tailwind config + images

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,19 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        alpineBlue: '#7DC1D2',
-        vitePurple: '#BD34FE',
-        tailwind: '#16BDCA'
-      }
+        chironLegendColor: '#2E4A55',
+        veyronSuperSportColor: '#E57F33',
+        centodieciColor: '#F24242',
+        leVoitureNoireColor: '#37373B',
+        divoColor: '#2DA9C1'
+      },
+      backgroundImage: theme => ({
+        chironLegend: "url('./public/images/chiron-sport-les-legendes-du-ciel-d.png')",
+        veyronSuperSport: "url('./public/images/veyron-super-sport-d.png')",
+        centodieci: "url('./public/images/centodieci-d.png')",
+        leVoitureNoire: "url('./public/images/la-voiture-noire-d.png')",
+        divo: "url('./public/images/divo-d.png')"
+      })
     },
   },
   variants: {
