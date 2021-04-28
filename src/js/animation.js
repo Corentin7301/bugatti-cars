43d377e88e77c84fadd8e5c2ff79f46e73b77be6
corentinperroux <corentin7301@gmail.com>
2021-04-28 11:21:49 +0100

    add scroll icon top bottom

--- src/js/animation.js
+++ src/js/animation.js
@@ -8,26 +8,55 @@ gsap.registerPlugin(ScrollTrigger);
 ScrollTrigger.refresh()
 
 
-gsap.utils.toArray(".carContainer").forEach(function(e) {
+gsap.utils.toArray(".carContainer").forEach(function (e) {
     let names = e.querySelectorAll(".carNames");
     let cars = e.querySelectorAll(".car");
     let desc = e.querySelectorAll(".carDesc");
     let logo = e.querySelector(".buggatiLogo");
+    let firstScrollBottom = e.querySelector(".firstScrollBottom");
+    
 
     let tl = gsap.timeline({
-        scrollTrigger: {
-            trigger: e,
-            pin: e,
-            scrub: 0.5,
-            start: "top top",
-            end: "+=200%",
-            // markers:true
-        }
-    })
-    .from(names, {xPercent: -150, duration: 1, ease:'power2.out', stagger:0.6},0)
-    .from(cars, {xPercent: 160, duration: 0.8, ease:'power2.out', stagger:0.6},0)
-    .from(desc, {xPercent: -150, duration: 1.5, ease:'power2.out', stagger:0.6},0)
-    .from(logo, {yPercent: 100, duration: 2, ease:'power2.out', stagger:0.6},0)
-    .to({}, {duration:0.3})
-    
+            scrollTrigger: {
+                trigger: e,
+                pin: e,
+                scrub: 0.5,
+                start: "top top",
+                end: "+=200%",
+                // markers:true
+            }
+        })
+        .from(names, {
+            xPercent: -150,
+            duration: 1,
+            ease: 'power2.out',
+            stagger: 0.6
+        }, 0)
+        .from(cars, {
+            xPercent: 160,
+            duration: 0.8,
+            ease: 'power2.out',
+            stagger: 0.6
+        }, 0)
+        .from(desc, {
+            xPercent: -150,
+            duration: 1.5,
+            ease: 'power2.out',
+            stagger: 0.6
+        }, 0)
+        .from(logo, {
+            yPercent: 100,
+            duration: 2,
+            ease: 'power2.out',
+            stagger: 0.6
+        }, 0)
+        .from(firstScrollBottom, {
+            xPercent: 150,
+            duration: 5,
+            ease: 'power2.out',
+            stagger: 0.6
+        }, 0)
+        .to({}, {
+            duration: 0.3
+        })
 })
